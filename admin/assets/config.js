c1940ffaf78f9a70cc0aed07341e2abd0b2c24c3
kaARAss <toyoma0808@gmail.com>
2026-08-20 02:55:18 +0500

    Update config.js

--- admin/assets/config.js
+++ admin/assets/config.js
@@ -5,7 +5,7 @@
  * Пример: "https://functions.yandexcloud.net/d4xxxxxxxxxxxxxxxxxx"
  */
 window.BULLBIKE_ADMIN = {
-  API_BASE: "",
+  API_BASE: "https://functions.yandexcloud.net/d4elnkci0oaltssrj058",
   // Путь, куда складываются загруженные фото (внутри репозитория).
   MEDIA_FOLDER: "images"
 };
